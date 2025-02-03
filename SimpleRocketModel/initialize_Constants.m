efe9f0a890f73a0849b0faa798588105df30eace
Jonathan <jonnovdriet@gmail.com>
2025-02-03 15:31:42 +0200

    Implemented East UP position, issue where rocket decelerates in x pos drift when no thrust

--- SimpleRocketModel/initialize_Constants.m
+++ SimpleRocketModel/initialize_Constants.m
@@ -1,24 +1,21 @@
-%Initialize constants for RCAM simulation
+% Initialize constants for RCAM simulation
 clear
 clc
 close all
 
 %% Define Constants
 x0 = [0;
-    0;
-    0;
-    90*pi/180];
-
-u = [100;
+      0;
+      0;
+      90*pi/180;
+      0;
       0];
+
   
-
-TF = 10;
-
+TF = 20;
 
 %% Run the model
 mdl = 'RocketSimulation.slx';
-sim(mdl)
 
 %% Plot the results
 out = sim(mdl);
@@ -29,43 +26,40 @@ u1 = out.simU.Data(:,1);  % Thrust (N)
 u2 = out.simU.Data(:,2);  % Nozzle pitch angle (rad)
 
 % Instantaneous States
-u    = out.simX.Data(:,1);  % Body x-axis velocity (m/s)
-v    = out.simX.Data(:,2);  % Body y-axis velocity (m/s)
-q    = out.simX.Data(:,3);  % Pitch rate (rad/s)
-theta= out.simX.Data(:,4);  % Pitch angle (rad)
+u     = out.simX.Data(:,1);  % Body x-axis velocity (m/s)
+v     = out.simX.Data(:,2);  % Body y-axis velocity (m/s)
+q     = out.simX.Data(:,3);  % Pitch rate (rad/s)
+theta = out.simX.Data(:,4);  % Pitch angle (rad)
 
-% Integrated States (e.g., positions)
-% NOTE: Verify that your simulation outputs integrated states in these columns.
-xpos = out.simX.Data(:,1);  % Integrated x-position (m)
-xypos= out.simX.Data(:,2);  % Integrated y-position (m)
+% Integrated States
 
+% the integrated positions should be in columns 5 and 6.
+xpos  = out.simX.Data(:,5);  % Integrated x-position (m)
+ypos = out.simX.Data(:,6);  % Integrated y-position (m)
+
+%% Plot Control Inputs
 figure
-
-%% Plot u1 (Thrust)
 subplot(2,1,1)
-plot(t, u1, 'LineWidth',1.5)
+plot(t, u1, 'LineWidth', 1.5)
 xlabel('Time (s)')
 ylabel('Thrust (N)')
 title('Control Input u_1: Thrust')
 grid on
 legend('u_1')
 
-%% Plot u2 (Nozzle Pitch)
 subplot(2,1,2)
-plot(t, u2, 'LineWidth',1.5)
+plot(t, u2, 'LineWidth', 1.5)
 xlabel('Time (s)')
 ylabel('Nozzle Pitch (rad)')
 title('Control Input u_2: Nozzle Pitch')
 grid on
 legend('u_2')
 
-
-
 %% Plot Instantaneous States
 figure
 % u: Body x-axis velocity
-subplot(2, 2, 1)
-plot(t, u, 'LineWidth',1.5)
+subplot(3, 2, 1)
+plot(t, u, 'LineWidth', 1.5)
 xlabel('Time (s)')
 ylabel('u (m/s)')
 title('State x_1: Body x-axis Velocity')
@@ -73,8 +67,8 @@ grid on
 legend('u')
 
 % v: Body y-axis velocity
-subplot(2, 2, 2)
-plot(t, v, 'LineWidth',1.5)
+subplot(3, 2, 2)
+plot(t, v, 'LineWidth', 1.5)
 xlabel('Time (s)')
 ylabel('v (m/s)')
 title('State x_2: Body y-axis Velocity')
@@ -82,8 +76,8 @@ grid on
 legend('v')
 
 % q: Pitch rate
-subplot(2, 2, 3)
-plot(t, q, 'LineWidth',1.5)
+subplot(3, 2, 3)
+plot(t, q, 'LineWidth', 1.5)
 xlabel('Time (s)')
 ylabel('q (rad/s)')
 title('State x_3: Pitch Rate')
@@ -91,10 +85,42 @@ grid on
 legend('q')
 
 % theta: Pitch angle
-subplot(2, 2, 4)
-plot(t, theta, 'LineWidth',1.5)
+subplot(3, 2, 4)
+plot(t, theta, 'LineWidth', 1.5)
 xlabel('Time (s)')
 ylabel('\theta (rad)')
 title('State x_4: Pitch Angle')
 grid on
 legend('theta')
+
+% u: EARTH x-axis pos
+subplot(3, 2, 5)
+plot(t, xpos, 'LineWidth', 1.5)
+xlabel('Time (s)')
+ylabel('u (m/s)')
+title('State x_1: Earth x-axis pos')
+grid on
+legend('u')
+
+% v: EARTH y-axis pos
+subplot(3, 2, 6)
+plot(t, ypos, 'LineWidth', 1.5)
+xlabel('Time (s)')
+ylabel('v (m/s)')
+title('State x_2: Earth y-axis pos')
+grid on
+legend('v')
+
+
+%% Plot Trajectory
+figure
+
+% trajectory
+subplot(1, 1, 1)
+plot(xpos, ypos, 'LineWidth', 1.5)
+xlabel('X position')
+ylabel('Y position')
+title('Trajectory')
+grid on
+legend('trajectory')
+axis equal
