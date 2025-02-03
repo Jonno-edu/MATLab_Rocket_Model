1bf0733d5c7ab04822d44ef43608f6d69761fbb2
Jonathan <jonnovdriet@gmail.com>
2025-02-03 11:13:18 +0200

    Completed 2D rocket linear equations, returning only body frame values: XDOT as: u, v, q, theta Added plots for XDOT

--- SimpleRocketModel/RocketModel.m
+++ SimpleRocketModel/RocketModel.m
@@ -1,115 +1,81 @@
 function [XDOT] = RocketModel(X, U)
-
-% Visualization of Both Frames:
-% ---------------------------------
-%
-%   Earth Frame (Inertial)
-%         ^ Y (Up, Altitude)
-%         |  
-%         |  
-%         o-------> X (East)
-%tes
-%   Body Frame (rotates with rocket)
-%          ^
-%      Y_b |   
-%          |-----> X_b  (Rocket forward)
-%
-
-x1 = X(1); % u - velocity along body x-axis
-x2 = X(2); % v - velocity along body y-axis
-x5 = X(3); % q - pitch rate
-x8 = X(4); % theta - pitch angle
-
-
-%Extract control vectors
-u1 = U(1); % d_A thrust
-u2 = U(2); % d_A z axis nozzle pitch
-
-%CONSTANTS
-m = 5; % Rocket total mass (kg)
-
-
-%Centre of gravity coordinates
-l_p = 1; % The distance from aerodynamic centre to centre of mass
-l = 1.5; % Distance from centre of mass to nozzle
-
-
-%Environmental constants
-rho = 1.225; % Air density (kg/m^3)
-g = 9.81; % Gravitational accelleration (m/s^2)
-
-
-%CONTROL LIMITS/SATURATION
-ulmin1 = 0; % min thrust (N)
-ulmax1 = 200; % max thrust (N)
-ulmin2 = -10 * pi / 180; % Min z axit nozzle pitch (rad)
-ulmax2 = 10 * pi / 180; % Max z axit nozzle pitch (rad)
-
-
-% Saturate control inputs
-if (u1 > ulmax1)
-    u1 = ulmax1;
-elseif (u1 < ulmin1)
-    u1 = ulmin1;
+    % Visualization of Both Frames:
+    % ---------------------------------
+    %
+    %   Earth Frame (Inertial)
+    %         ^ Y (Up, Altitude)
+    %         |  
+    %         |  
+    %         o-------> X (East)
+    %
+    %   Body Frame (rotates with rocket)
+    %          ^
+    %      Y_b |   
+    %          |-----> X_b  (Rocket forward)
+    
+    % State Variables
+    u = X(1);       % Velocity along body x-axis 
+    v = X(2);       % Velocity along body y-axis
+    q = X(3);       % Pitch rate
+    theta = X(4);   % Pitch angle
+    
+    % Extract control inputs
+    thrust = U(1);          % Thrust magnitude (N)
+    nozzlePitch = U(2);     % Nozzle pitch angle (rad)
+    
+    % CONSTANTS
+    m = 2;      % Rocket total mass (kg)
+    l_p = 1;    % Distance from aerodynamic centre to centre of mass
+    l   = 1.5;  % Distance from centre of mass to nozzle
+    
+    % Environmental constants
+    rho = 1.225; % Air density (kg/m^3)
+    g = 9.81;    % Gravitational acceleration (m/s^2)
+    
+    % CONTROL LIMITS/SATURATION
+    thrust_min = 0;         % min thrust (N)
+    thrust_max = 500;       % max thrust (N)
+    nozzle_min = -10 * pi / 180; % Min nozzle pitch (rad)
+    nozzle_max = 10 * pi / 180;  % Max nozzle pitch (rad)
+    
+    % Saturate control inputs
+    thrust = min(max(thrust, thrust_min), thrust_max);
+    nozzlePitch = min(max(nozzlePitch, nozzle_min), nozzle_max);
+    
+    % Inertia Matrix for 2D (simplified)
+    Ib = m * 40.07;  % Example inertia value
+    invIb = 1 / Ib;
+    
+    % THRUST FORCE AND MOMENT
+    % Use nozzle pitch angle for thrust direction
+    gamma = nozzlePitch;  
+    Fp = thrust;  % Thrust magnitude (N)
+    Fp_b = Fp * [cos(gamma); sin(gamma)];
+    
+    % GRAVITY EFFECTS in Body Frame
+    % Convert gravity to body frame using pitch angle theta
+    g_b = [-g * sin(theta); 
+            g * cos(theta)];
+    Fg_b = m * g_b;
+    
+    % Net Force in the Body Frame
+    F_b = Fp_b + Fg_b;
+    
+    % Compute accelerations (Newton's Second Law)
+    udot = F_b(1) / m;
+    vdot = F_b(2) / m;
+    
+    % Angular Dynamics (Moment about centre of gravity)
+    % Here we assume moment arm is l and only the y-component of thrust causes moment.
+    Mcg_b = l * Fp_b(2);
+    qdot = invIb * Mcg_b;
+    
+    % Theta dot is the pitch rate
+    thetadot = q;
+    
+    % Return state derivatives in first-order form
+    XDOT = [udot;
+            vdot;
+            qdot;
+            thetadot];
 end
-
-if (u2 > ulmax2)
-    u2 = ulmax2;
-elseif (u2 < ulmin2)
-    u2 = ulmin2;
-end
-
-
-% INTERMEDIATE VARIABLES
-
-
-%Define vectors wbe_b and V_b
-wbe_b = [x5]; % Angular velocity vector in body frame
-V_b = [x1; x2]; % Velocity vector in body frame
-
-
-%Inertia Matrix for 2D
-Ib = m * 40.07; % Simplified inertia for 2D (example value)
-invIb = 1 / Ib; % Inverse of inertia
-
-
-% THRUST FORCE AND MOMENT
-%Thrust
-gamma1 = u1;
-
-Fp = u1; % Thrust (N)
-
-Fp_b = Fp * [cos(gamma1); 
-             sin(gamma1)];
-
-% GRAVITY EFFECTS
-g_b = [-g * sin(x8);
-        g * cos(x8)];
-
-Fg_b = m * g_b; % Gravitational force in body frame
-
-
-%Form F_b (all forces in Fb) and calc udot, vdot, wdot
-F_b = Fg_b + Fp_b; % Gravity force vector, Thrust force vector
-
-% Simplified cross product for 2D
-cross_wbe_b_V_b = wbe_b * V_b(2) - wbe_b * V_b(1);
-
-% Split x1to3dot into separate calculations
-udot = (1 / m) * F_b(1) - cross_wbe_b_V_b; % Translational acceleration in x direction
-vdot = (1 / m) * F_b(2) - cross_wbe_b_V_b; % Translational acceleration in y direction
-
-x1to3dot = [udot; vdot]; % Combine into a vector
-
-%Form Mcg_b (All moments about CoG in Fb) and calc qdot
-Mcg_b = l_p * Fp_b(2); % Moment due to thrust (simplified for 2D)
-x4dot = invIb * (Mcg_b - wbe_b * Ib * wbe_b); % Rotational acceleration (qdot)
-
-%Calculate thetadot
-x5dot = x5; % Angular rate (thetadot)
-
-%Place in first order form
-XDOT = [x1to3dot;
-        x4dot;
-        x5dot]; % State derivatives
-
--- SimpleRocketModel/initialize_Constants.m
+++ SimpleRocketModel/initialize_Constants.m
@@ -7,14 +7,94 @@ close all
 x0 = [0;
     0;
     0;
-    90 * pi/180];
+    90*pi/180];
 
-u = [200;
-    0];
+u = [100;
+      0];
+  
 
-
-TF = 3*60;
+TF = 10;
 
 
 %% Run the model
-sim('RocketSimulation.slx')
+mdl = 'RocketSimulation.slx';
+sim(mdl)
+
+%% Plot the results
+out = sim(mdl);
+t = out.simX.Time;
+
+% Control Inputs
+u1 = out.simU.Data(:,1);  % Thrust (N)
+u2 = out.simU.Data(:,2);  % Nozzle pitch angle (rad)
+
+% Instantaneous States
+u    = out.simX.Data(:,1);  % Body x-axis velocity (m/s)
+v    = out.simX.Data(:,2);  % Body y-axis velocity (m/s)
+q    = out.simX.Data(:,3);  % Pitch rate (rad/s)
+theta= out.simX.Data(:,4);  % Pitch angle (rad)
+
+% Integrated States (e.g., positions)
+% NOTE: Verify that your simulation outputs integrated states in these columns.
+xpos = out.simX.Data(:,1);  % Integrated x-position (m)
+xypos= out.simX.Data(:,2);  % Integrated y-position (m)
+
+figure
+
+%% Plot u1 (Thrust)
+subplot(2,1,1)
+plot(t, u1, 'LineWidth',1.5)
+xlabel('Time (s)')
+ylabel('Thrust (N)')
+title('Control Input u_1: Thrust')
+grid on
+legend('u_1')
+
+%% Plot u2 (Nozzle Pitch)
+subplot(2,1,2)
+plot(t, u2, 'LineWidth',1.5)
+xlabel('Time (s)')
+ylabel('Nozzle Pitch (rad)')
+title('Control Input u_2: Nozzle Pitch')
+grid on
+legend('u_2')
+
+
+
+%% Plot Instantaneous States
+figure
+% u: Body x-axis velocity
+subplot(2, 2, 1)
+plot(t, u, 'LineWidth',1.5)
+xlabel('Time (s)')
+ylabel('u (m/s)')
+title('State x_1: Body x-axis Velocity')
+grid on
+legend('u')
+
+% v: Body y-axis velocity
+subplot(2, 2, 2)
+plot(t, v, 'LineWidth',1.5)
+xlabel('Time (s)')
+ylabel('v (m/s)')
+title('State x_2: Body y-axis Velocity')
+grid on
+legend('v')
+
+% q: Pitch rate
+subplot(2, 2, 3)
+plot(t, q, 'LineWidth',1.5)
+xlabel('Time (s)')
+ylabel('q (rad/s)')
+title('State x_3: Pitch Rate')
+grid on
+legend('q')
+
+% theta: Pitch angle
+subplot(2, 2, 4)
+plot(t, theta, 'LineWidth',1.5)
+xlabel('Time (s)')
+ylabel('\theta (rad)')
+title('State x_4: Pitch Angle')
+grid on
+legend('theta')
