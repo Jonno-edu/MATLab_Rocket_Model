0e8936dee96ab06558295f3a71454e63bec2c61f
Jonathan <jonnovdriet@gmail.com>
2025-02-24 16:36:44 +0200

    Created Major Subsystems and layout for STEVE

---
+++ STEVE_Simulator/initialize_parameters.m
@@ -0,0 +1,9 @@
+%% Nozzle Actuators
+wn_act = 1; % rad/s (update for actuators)
+z_act = 0.3;
+maxdef_nozzle = (30) * pi/180; % rad
+mindef_nozzle = -maxdef_nozzle;
+rate_lim_nozzle = (500) * pi/180; % rad
+
+max_thrust = 1000; % N
+
