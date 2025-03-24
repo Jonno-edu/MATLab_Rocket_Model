e05d20b63fd15eadc9649ff977a0e26e3f9bf738
Jonathan <jonnovdriet@gmail.com>
2025-03-24 12:43:07 +0200

    Fixed thrust vector moment calculation and added calculated control system for pitch rate

--- GimbalThrustModel.m
+++ GimbalThrustModel.m
@@ -14,17 +14,17 @@ classdef GimbalThrustModel < matlab.System
     end
 
     methods (Access = protected)
-        function setupImpl(obj)
+        function setupImpl()
             % Perform one-time calculations, such as computing constants
         end
 
-        function [thrust_x, thrust_z, moment] = stepImpl(obj,thrust, nozzle_angle)
+        function [thrust_x, thrust_z, moment] = stepImpl(thrust, nozzle_angle, CG)
             thrust_x = thrust * cos(nozzle_angle);
             thrust_z = thrust * sin(nozzle_angle);
-            moment = obj.nozzleOffset*thrust_z;
+            moment = CG*thrust_z;
         end
 
-        function resetImpl(obj)
+        function resetImpl()
             % Initialize / reset internal properties
         end
     end
